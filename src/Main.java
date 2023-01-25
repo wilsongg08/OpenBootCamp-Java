bc5c38d2fe60888ed20d01af26b5bb039450b266
Wilson Gómez <wilson.gomez@segurosbolivar.com>
2023-01-25 17:42:41 -0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte variableByte = 20;
+        short variableShort = 10;
+        int variableInt = 50;
+        long variableLong = 6501;
+        float variableFloat = 50.06f;
+        double variableDouble = 95.07d;
+        boolean variableBoolean = true;
+
+
+        System.out.println(variableByte);
+        System.out.println(variableShort);
+        System.out.println(variableInt);
+        System.out.println(variableLong);
+        System.out.println(variableFloat);
+        System.out.println(variableDouble);
+        System.out.println(variableBoolean);
+    }
+}
